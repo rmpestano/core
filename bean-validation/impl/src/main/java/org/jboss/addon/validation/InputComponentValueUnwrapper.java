5d05f9be2a37afe177c90c43350de47ab06f23a5
George Gastaldi <gegastaldi@gmail.com>
2014-01-20 16:12:30 -0200

    Added changes suggested by Gunnar

--- bean-validation/impl/src/main/java/org/jboss/addon/validation/InputComponentValueUnwrapper.java
+++ bean-validation/impl/src/main/java/org/jboss/addon/validation/InputComponentValueUnwrapper.java
@@ -12,12 +12,16 @@ import java.lang.reflect.Type;
 import org.hibernate.validator.spi.valuehandling.ValidatedValueUnwrapper;
 import org.jboss.forge.addon.ui.input.UIInput;
 
+import com.fasterxml.classmate.ResolvedType;
+import com.fasterxml.classmate.TypeResolver;
+
 /**
  * 
  * @author <a href="ggastald@redhat.com">George Gastaldi</a>
  */
 public class InputComponentValueUnwrapper extends ValidatedValueUnwrapper<UIInput<?>>
 {
+   private final TypeResolver typeResolver = new TypeResolver();
 
    @Override
    public Object handleValidatedValue(UIInput<?> value)
@@ -28,7 +32,8 @@ public class InputComponentValueUnwrapper extends ValidatedValueUnwrapper<UIInpu
    @Override
    public Type getValidatedValueType(Type valueType)
    {
-      return valueType;
+      ResolvedType resolvedType = typeResolver.resolve(valueType);
+      return resolvedType.typeParametersFor(UIInput.class).get(0).getErasedType();
    }
 
 }
